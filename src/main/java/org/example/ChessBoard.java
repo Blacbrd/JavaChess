a2587e4bde2ea6fb9181fc81dad63f2f46af5048
Blacbrd <blacbrd123@gmail.com>
2024-09-04 20:09:49 +0100

    Finished creating the board with Graphics2D package

--- src/main/java/org/example/ChessBoard.java
+++ src/main/java/org/example/ChessBoard.java
@@ -8,20 +8,38 @@ public class ChessBoard extends JPanel {
 
     private final int tileSize = 85;
 
-    public ChessBoard() {
+    private final int columns = 8;
+    private final int rows = 8;
 
-        int columns = 8;
-        int rows = 8;
+    public ChessBoard() {
 
         // Similar to "set size", however we have a layout manager (flow layout, along with minimum size)
         // So, setPreferredSize will give the initial size, while minimum size will only allow the user to make the window that small
-        this.setPreferredSize(new Dimension(columns + tileSize, rows + tileSize));
-
-        this.setBackground(Color.gray);
+        this.setPreferredSize(new Dimension(columns * tileSize, rows * tileSize));
 
     }
 
     public int getTileSize() {
         return tileSize;
+    }
+
+    public void paintComponent(Graphics g){
+
+        // Like turtle in Python, allows us to draw/paint in GUI
+        Graphics2D graphics2D = (Graphics2D) g;
+
+        for(int i = 0; i < rows; i++){
+
+            for(int j = 0; j < columns; j++){
+
+                // Alternates, if i + j is even, colour is white, else black
+                graphics2D.setColor((i + j) % 2 == 0 ? new Color(17, 0, 255) : new Color(130, 194, 255));
+
+                graphics2D.fillRect(tileSize * j, tileSize * i, tileSize, tileSize);
+
+            }
+
+        }
+
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,14 +13,23 @@ public class Main {
         // Allows to close using x button
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Adds title to the window
+        frame.setTitle("Java Chess");
+
+        // This layout allows us full control of where we put our GUI elements as each grid can be a different size
+        frame.setLayout(new GridBagLayout());
+
+        // Initialise chessBoard class
+        ChessBoard chessBoard = new ChessBoard();
+
+        frame.setBackground(Color.black);
+
         // Sets a minimum size for our window, the Dimension class allows us to define (width, height)
         // Cannot reduce window size smaller than these dimensions
-        frame.setMinimumSize(new Dimension(1000, 1000));
+        frame.setMinimumSize(new Dimension(1200, 1200));
 
         // Has the window open in the middle of the screen
         frame.setLocationRelativeTo(null);
-
-        ChessBoard chessBoard = new ChessBoard();
 
         frame.add(chessBoard);
 
